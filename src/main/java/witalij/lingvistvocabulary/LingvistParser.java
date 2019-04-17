3686de1610848a9e7a18c707512ff9b00d6c4a52
vitalijbukatkin <vitaliy.bukatkin@gmail.com>
2019-04-17 23:36:24 +0300

    Update package structure Add to pom.xml jar plugin

--- src/main/java/witalij/lingvistvocabulary/LingvistParser.java
+++ src/main/java/witalij/lingvistvocabulary/LingvistParser.java
@@ -1,3 +1,5 @@
+package witalij.lingvistvocabulary;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
--- src/main/java/witalij/lingvistvocabulary/LingvistUI.java
+++ src/main/java/witalij/lingvistvocabulary/LingvistUI.java
@@ -1,3 +1,5 @@
+package witalij.lingvistvocabulary;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
